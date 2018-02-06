274be77030fe0b58980cbb3bd5861a02e34a70d5
John E Shepler Jr <john.sheplerjr@gmail.com>
2018-02-05 23:34:46 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const electron = require('electron');
+const { app, BrowserWindow, Menu } = electron;
+
+let mainWindow;
+let addWindow;
+app.on('ready', ()=> {
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(`file://${__dirname}/main.html`);
+    mainWindow.on('closed', ()=> app.quit())
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu)
+});
+
+function createAddWindow(){
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add New Todo'
+    });
+    addWindow.loadURL(`file://${__dirname}/add.html`)
+}
+
+const menuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'New Todo',
+                click(){
+                    createAddWindow();
+                }
+            }, {
+                label: 'Quit',
+                // Following line is an alternative to the IIFE
+                // accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q'
+                accelerator: (()=>{
+                    if (process.platform === "darwin") {
+                        return 'Command+Q'
+                    } else {
+                        return 'Ctrl+Q'
+                    }
+                })(),
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+if (process.platform === 'darwin'){
+    menuTemplate.unshift({})
+}
